e814703f736dd962f1fcbcaf2f3a7f8f61b86643
KathrynAlta <you@example.com>
2025-02-05 16:54:17 -0700

    start your own DAG

---
+++ Gannon_DAG/01_Create_DAG.R
@@ -0,0 +1,13 @@
+# Constructing DAG #
+
+# Write out the relationships
+
+"O2 Amount at Freeze" -> "Anox Duration"
+  "Lake Volume" -> "O2 Amount at Freeze"
+  "Fall Mixing" -> "O2 Amount at Freeze"
+  "Freezing Degree Days" ->
+
+"Ice Duration" -> "Anox Duration"
+
+"Rate of O2 Draw Down" -> "Anox Duration"
+
