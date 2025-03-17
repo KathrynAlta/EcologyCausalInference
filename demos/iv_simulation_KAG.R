6de9d44cf4b42a8cc3d7a33c2637165d48271688
Your Name <kgannon59401@gmail.com>
2025-03-17 08:52:25 -0600

    Desktop

--- demos/iv_simulation_KAG.R
+++ demos/iv_simulation_KAG.R
@@ -29,43 +29,49 @@ library(lavaan)
 #### simulations
 set.seed(0)
 # first generate exogeneous variables: o and z
-z <- rnorm(100, 3, 7)
+z <- rnorm(100, 3, 7) # Exogenous like exo-planet! Coming from outside the system
 o <- rnorm(100, 3, 5)
 u <- rnorm(100, 0, 7)  # assuming random error and uncorrelated by generating this structure
 
-# now generate endogenous variables
-#assume it depends linearly on o and z with some noise and with some coefficients
-# if I made the coefficient on z and, depending on the noise (if small), then its a strong instrument
-# bc most of x will be explained by z. #how much can we tell about x from z?
-beta_zx = 5
-beta_ox = 5  #size of bias from omitted variable o depends on this coefficient as well as beta_oy (they compound)
-x  <- beta_zx * z + beta_ox*o + rnorm(100, 0, 1)
+# now generate endogenous variables (x and y)
+# assume it depends linearly on o and z with some noise and with some coefficients
 
-#generate y, the dependent variable
-beta_xy = 1
-beta_oy = 50
-y = beta_xy*x + beta_oy*o + u
+    # if I made the coefficient on z and, depending on the noise (if small), then its a strong instrument
+    # bc most of x will be explained by z. #how much can we tell about x from z?
+    beta_zx = 5
+    beta_ox = 5  #size of bias from omitted variable o depends on this coefficient as well as beta_oy (they compound)
+        # This is why having a "bad" instrumental variable can actually be worse! you are just adding more bias
+    x  <- beta_zx * z + beta_ox*o + rnorm(100, 0, 1)
+
+    #generate y, the dependent variable
+    beta_xy = 1
+    beta_oy = 50
+    y = beta_xy*x + beta_oy*o + u
 
 ### ENDOGENIETY: OMMITED VARIABLES BIAS OF o ###
 ## Now let's just do a linear model of x --> y
 OLS <- lm(y ~ x)
 coeftest(OLS)  #biased up, because positive effect of o on y and on x.
+    # This comes out as 4.9, but we set it to 1! That is because there is bias in this ommitted variable
 
 #Now let's do a model where we control for o., assuming you observe o.
 OLS2 <- lm(y ~ x + o)
 coeftest(OLS2)
+    # Now we do way better! Now we are estimating 1.01
+    # But ... that is assuming that we actually measured the ommitted variable ... not always the case
 
 # Third model: IV; we don't observe o but we have data on z.
 iv <- ivreg(y ~ x | z)
-summary(iv)
+summary(iv)  # Enter the hero instrumental variable! We still over estimate (comes out to about 2) but way better thansummarysummary(iv)              # normal OLS and does NOT require us to know anything about o
+summary(iv, diagnostics = T)
 
 ## test that beta_xy = 1
 # with iv not significantly different than true value (which = 1); rhs is specifying that hypothesis that the true val =1
-linearHypothesis(iv, c(0,1), rhs=1)
+linearHypothesis(iv, c(0,1), rhs=1) #? Help?
 # issue iv gives a lot of noise but not significantly diff than real value
 
 # with the OLS model with an omitted variable; it is significantly different.
-linearHypothesis(OLS, c(0,1), rhs=1)
+linearHypothesis(OLS, c(0,1), rhs=1)  # OLS vlaue is significantly different from 1
 
 ############################################################################
 ## Stepping into reverse causality ########################################
@@ -84,7 +90,7 @@ linearHypothesis(OLS, c(0,1), rhs=1)
 #   (2) Y2 = g0 + g1*Y1 + g2*X2 + e2
 # where a0,a1,a2,g0 and g1 are parameters that we specify explicitly while e1 and e2 are error terms.
 #
-# We can re-write the system of equations like this using simple substitution:
+# We can re-write the system of equations like this using simple substitution: Algebra
 #   Y1 = (a0 + a1*g0 + a1*e2 + a2*x1 + a1*g2*x2 + e1)/(1 – a1*g1)
 #   Y2 = (g0 + g1*a0 + g1*a2*x1 + g1*e1 + g2*x2 + e2)/(1 – g1*a1)
 # and use this to simulate the data
@@ -94,7 +100,7 @@ n <- 1000
 
 # Set up parameters
 a0 <- 1
-a1 <- 0.1
+a1 <- 0.1 #Weak!
 # a1 = 1 # Determines strength of simultaneity in Equation (1); close to 0 is weak
 a2 <- 1      # Determines how strong of an instrument x2 is for Equation (2); close to 0 is weak
 g0 <- 5
